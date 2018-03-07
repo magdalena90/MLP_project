4af6835c526bfaa44b68368615e535c668ff9c2a
Pablo Campos <pjcv89@gmail.com>
2018-03-07 17:14:19 +0000

    added preprocessing with downloading by wnid

---
+++ preprocessing_by_wnid.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+
+# Pix - clone pix2pix repo? 1 if so
+# nImages - number of Images to download
+# nTest - number of Images to choose as validation set
+
+nImages=$1
+nTest=$2
+wnID=$3
+nPixel=$4
+
+printf 'ACTIVATING CONDA ENVIRONMENT \n'
+source /home/$USER/miniconda3/bin/activate py27
+
+printf 'DOWNLOADING SAMPLE IMAGES \n'
+python download_sample_images_by_wnid.py $nImages $wnID Images
+
+printf 'PREPROCESSING SAMPLE IMAGES \n'
+python process_images.py Images ImagesOut $nImages $nPixel $nPixel train
+
+mv ImagesOut/* pix2pix-tensorflow/datasets/imagenet/train
+rm -r ImagesOut
+
+printf 'ENUMERATING IMAGES \n'
+cd pix2pix-tensorflow/datasets/imagenet/train
+counter=0
+for file in *; do 
+    [[ -f $file ]] && mv -i "$file" $((counter+1)).jpg && ((counter++))
+done
+
+cd ..
+printf 'SELECTING VALIDATION IMAGES \n'
+shuf -n $nTest -e train/* | xargs -i mv {} val
+printf 'DONE \n'
