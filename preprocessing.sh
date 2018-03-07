b378b08a91411bd62e60f08f698f6dfa153e7cfc
Pablo Campos <pjcv89@gmail.com>
2018-03-07 16:29:22 +0000

    added nPixel flag

--- preprocessing.sh
+++ preprocessing.sh
@@ -3,10 +3,12 @@
 # Pix - clone pix2pix repo? 1 if so
 # nImages - number of Images to download
 # nTest - number of Images to choose as validation set
+# nPixel - number of pixels of the images
 
 Pix=$1
 nImages=$2
 nTest=$3
+nPixel=$4
 
 if [ $Pix -eq 1 ];then
         printf 'CLONNING pix2pix \n'
@@ -34,7 +36,7 @@ source /home/$USER/miniconda3/bin/activate py27
 printf 'DOWNLOADING SAMPLE IMAGES \n'
 python download_sample_images.py $nImages Images False False
 printf 'PREPROCESSING SAMPLE IMAGES \n'
-python process_images.py Images ImagesOut $nImages 256 256 train
+python process_images.py Images ImagesOut $nImages $nPixel $nPixel train
 
 mv ImagesOut/* pix2pix-tensorflow/datasets/imagenet/train
 rm -r ImagesOut
