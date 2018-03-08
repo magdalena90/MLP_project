17d57f531dff245b1012bd0a4a9ca145d246f72f
Pablo Campos <pjcv89@gmail.com>
2018-03-08 20:00:15 +0000

    modified default epoch and batch_size flags

--- gpu_training.sh
+++ gpu_training.sh
@@ -31,4 +31,4 @@ source /home/${STUDENT_ID}/miniconda3/bin/activate py27
 
 cd pix2pix-tensorflow
 
-python main.py --dataset_name imagenet --phase train --epoch 150 --batch_size 20 --fine_size 128
+python main.py --dataset_name imagenet --phase train --epoch 250 --batch_size 5 --fine_size 128
