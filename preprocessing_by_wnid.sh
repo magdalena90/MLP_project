097cc63ea3a19ca7c2ef116bcbeadfb3853d6193
Pablo Campos <pjcv89@gmail.com>
2018-03-08 18:46:41 +0000

    removed enumerating step

--- preprocessing_by_wnid.sh
+++ preprocessing_by_wnid.sh
@@ -41,14 +41,8 @@ python process_images.py Images ImagesOut $nImages $nPixel $nPixel train
 mv ImagesOut/* pix2pix-tensorflow/datasets/imagenet/train
 rm -r ImagesOut
 
-printf 'ENUMERATING IMAGES \n'
-cd pix2pix-tensorflow/datasets/imagenet/train
-counter=0
-for file in *; do 
-    [[ -f $file ]] && mv -i "$file" $((counter+1)).jpg && ((counter++))
-done
+cd pix2pix-tensorflow/datasets/imagenet
 
-cd ..
 printf 'SELECTING VALIDATION IMAGES \n'
 shuf -n $nTest -e train/* | xargs -i mv {} val
 printf 'DONE \n'
